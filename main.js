d44df25d82b2fa862b7034681d3afc04bb00e2f1
Paloma <145389683+p-alom-a@users.noreply.github.com>
2024-08-02 10:09:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import pub from './pub-database.js';
+//import pub from './pub-database.js';
 import holes from './parcours-database.js';
 
 (function() {
@@ -38,6 +38,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let satelliteMap = null;
     let currentMarker = null;
+
+    const pub = [
+    './pub1.jpg',
+    './pub2.jpg',
+    './pub3.jpg',
+    './pub4.jpg',
+    './pub5.jpg',
+    './pub6.jpg',
+    './pub7.jpg',
+    './pub8.jpg',
+    './pub9.jpg',
+    './pub10.jpg',
+    './pub11.jpg',
+    './pub12.jpg',
+    './pub13.jpg',
+    './pub14.jpg',
+    './pub15.jpg',
+    './pub16.jpg',
+    './pub17.jpg',
+    './pub18.jpg'
+];
 
     class Player {
         constructor(name, color) {
